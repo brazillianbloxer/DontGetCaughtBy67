0514a71a897a9a44afdf4072d1647d010ff207e9
heybloxer <ebcbdubshab@gmail.com>
2026-01-17 14:51:33 +0300

    Main Script (execute this)

---
+++ script.lua
@@ -0,0 +1,13 @@
+--[[
+__________                      .__.__  .__  .__                __________.__                             
+\______   \____________  _______|__|  | |  | |__|____    ____   \______   \  |   _______  ___ ___________ 
+ |    |  _/\_  __ \__  \ \___   /  |  | |  | |  \__  \  /    \   |    |  _/  |  /  _ \  \/  // __ \_  __ \
+ |    |   \ |  | \// __ \_/    /|  |  |_|  |_|  |/ __ \|   |  \  |    |   \  |_(  <_> >    <\  ___/|  | \/
+ |______  / |__|  (____  /_____ \__|____/____/__(____  /___|  /  |______  /____/\____/__/\_ \\___  >__|   
+        \/             \/      \/                    \/     \/          \/                 \/    \/        
+                         		   Welcome to Brazillian Bloxer Scripts!
+									Github: github.com/brazillianbloxer
+									 Don't Get Crushed by 67 Script
+]]--
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/brazillianbloxer/DontGetCaughtBy67/refs/heads/main/loader.lua"))()
